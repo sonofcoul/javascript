42954d426fe3470c3fd8408a945e2eb0d87fddd2
sonofcoul <alex-63100@hotmail.fr>
2021-11-23 12:48:43 +0100

    remise appliquée

--- calcul tva/index.js
+++ calcul tva/index.js
@@ -5,19 +5,39 @@ const TAUX_TVA = 20;
 let montantHT;
 let montantTVA;
 let montantTTC;
+let isRemise, tauxRemise;
+let textRemise;
 
 //affectation de valeur
-montantHT = parseFloat(prompt("Montant HT ?", 100));
+do {
+  montantHT = parseFloat(prompt("Montant HT ?", 100));
+} while (isNaN(montantHT));
+
+isRemise = prompt("Avez vous une remise ?");
+
+if (isRemise === "oui" || isRemise === "yes") {
+  do {
+    tauxRemise = parseFloat(
+      prompt("Quel est le taux de la remise ?").replace(",", ".")
+    );
+  } while (isNaN(tauxRemise));
+  montantHT -= (montantHT * tauxRemise) / 100;
+}
+
+if (isRemise === "oui" || isRemise === "yes") {
+  textRemise = `Une remise de ${tauxRemise} % a été appliquée sur le montant HT.`;
+} else {
+  textRemise = "Aucune remise n'a été appliquée.";
+}
 
 //calcul montant TVA
 montantTVA = (montantHT * TAUX_TVA) / 100;
 
 //calcul du montant TTC : HT + TVA
 montantTTC = montantHT + montantTVA;
-console.log(
-  `Pour un montant HT de ${montantHT}€, il y a ${montantTVA}€ de TVA. Le montant TTC est donc de ${montantTTC}€.`
-);
+
+//calcul remise
 
 document.querySelector(
   "#content"
-).innerHTML = `<p>Pour un montant HT de ${montantHT}€, il y a ${montantTVA}€ de TVA. <br /> Le montant TTC est donc de ${montantTTC}€.</p>`;
+).innerHTML = `<p>Pour un montant HT de ${montantHT}€, il y a ${montantTVA}€ de TVA. <br /> Le montant TTC est donc de ${montantTTC}€. <br /> ${textRemise}</p>`;
