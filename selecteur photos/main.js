a7fbd010dad9b330b2878c35a5b99538f81138c4
sonofcoul <alex-63100@hotmail.fr>
2021-11-26 09:44:19 +0100

    selecteur photos

--- selecteur photos/main.js
+++ selecteur photos/main.js
@@ -1,4 +1,17 @@
 "use strict";
+let count = 0;
+
+function displayCount() {
+  const em = document.querySelector("#total em");
+  em.textContent = count;
+  if (count == 0) {
+    em.style.color = "blue";
+  } else if (count == 10) {
+    em.style.color = "yellow";
+  } else {
+    em.style.color = "pink";
+  }
+}
 
 function selectOne() {
   this.classList.toggle("selected");
@@ -8,12 +21,24 @@ function selectOne() {
   document.querySelector("#total em").textContent = selected.length;
 }
 
-function selectAll() {}
+function selectAll() {
+  photos.forEach((li) => li.classList.add("selected"));
+  count = photos.length;
+  displayCount();
+}
 
-function deselectAll() {}
+function deselectAll() {
+  photos.forEach((li) => li.classList.remove("selected"));
+  count = 0;
+  displayCount();
+}
 
 let photos = document.querySelectorAll(".photo-list li");
 
 photos.forEach((li) => {
   li.addEventListener("click", selectOne);
 });
+
+document.querySelector("#selectAll").addEventListener("click", selectAll);
+
+document.querySelector("#deselectAll").addEventListener("click", deselectAll);
