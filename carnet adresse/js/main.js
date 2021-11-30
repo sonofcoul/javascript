48616ae610fb212bc53dee88f480ae2e38f0dc83
sonofcoul <alex-63100@hotmail.fr>
2021-11-30 21:29:32 +0100

    carnet

--- carnet adresse/js/main.js
+++ carnet adresse/js/main.js
@@ -1,37 +1,64 @@
 "use strict";
 
 let FORM;
-let notebook = new Array();
 
-function displayForm() {
+function loadDatas() {
+  let datas = JSON.parse(localStorage.getItem("notebook"));
+  if (datas === null) datas = [];
+  return datas;
+}
+
+function showForm() {
   FORM.removeClass("hide");
 }
 
-function rmvForm() {
+function hideForm() {
   FORM.addClass("hide");
 }
 
 function saveContact() {
+  let notebook = loadDatas();
   let contact = {
     title: $("#title").val(),
     lastname: $("#lastName").val().trim(),
     firstname: $("#firstName").val().trim(),
     phone: $("#phone").val().trim(),
   };
+
   notebook.push(contact);
   console.log(notebook);
+
+  let notebookJSON = JSON.stringify(notebook);
+  localStorage.setItem("notebook", notebookJSON);
+
+  displayNotebook();
+  FORM[0].reset();
+  FORM.addClass("hide");
 }
 
-let notebookJSON = JSON.stringify(notebook);
-localStorage.setItem("notebook", notebookJSON);
+function displayNotebook() {
+  let notebook = loadDatas();
 
-let test = localStorage.getItem("notebook");
-console.log(test);
+  $("#address-book").html("<ul></ul>");
+  notebook.forEach((line) => {
+    $("#address-book ul").append(`<li>${line.lastname} ${line.firstname}</li>`);
+  });
+}
+
+function displayContact() {
+  let details = $("#contacts-details");
+  details.removeClass("hide");
+  console.log("test");
+}
 
 $(document).ready(function () {
   FORM = $("#contact-form");
 
-  $("#add-contact").on("click", displayForm);
+  $("#add-contact").on("click", showForm);
 
   $("#save-contact").on("click", saveContact);
+
+  displayNotebook();
+
+  $("#contact-details").on("click", displayContact);
 });
